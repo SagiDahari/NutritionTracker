e136cebd71d7a47f533088b6104142628a14892a
Sagi Dahari <sagidahari7@gmail.com>
2025-12-25 15:53:07 +0200

    Changed dockerfile for depand db config to be ready for deployment

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,29 +3,40 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Prevent accidental test runs on production database
+// Prevent accidental test runs on development database
 if (process.env.NODE_ENV === 'test') {
   if (!process.env.DB_NAME || !process.env.DB_NAME.includes('test')) {
     console.log(`trying to access the db: ${process.env.DB_NAME} from the enviorment ${process.env.NODE_ENV}`)
     throw new Error('Test mode requires database name containing "test"');
   }
 }
-
-export const db = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let db;
+// Production 
+if (process.env.DATABASE_URL) {
+  db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+} else {
+  // Development and Testing
+  db = new pg.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 // Verify connection on startup
 db.on('connect', async (client) => {
   const result = await client.query('SELECT current_database()');
   const dbName = result.rows[0].current_database;
-  
+
   if (process.env.NODE_ENV === 'test' && !dbName.includes('test')) {
     console.error(`❌ CONNECTED TO WRONG DATABASE: ${dbName}`);
     process.exit(1);
   }
 });
+
+export { db }
